d5e8b91a2e42ad45b6c62994b681d4c233078e7c
Pete Cartwright <pete.cartwright@gmail.com>
2015-07-23 21:13:56 -0400

    complete database writing part

--- wishListChecker.py
+++ wishListChecker.py
@@ -3,28 +3,30 @@ import requests
 import re
 import sqlite3
 import sys
+import random
+import datetime
+from time import sleep
 
 #### NEXT UP
 
-# convert code to handle new DB layout
-# test, test, test
 
 # write mvp Django frontend
 
+## Nice to have - add dateAddedToWishlist from wishlist page - id="itemAction_****", then text of first span
 
 #TODO
-
-    # additional comments
 
     # Need
         # put this on petecartwright.com and schedule
         # set up email notification
-        # handle textbook pages. ex: http://www.amazon.com/Beautiful-Code-Leading-Programmers-Practice/dp/0596510047/
+        # rename functions to _ versions
+        # make sql column names consistent
 
     # Nice
         # paralell retrievals? (http://docs.python-requests.org/en/v0.10.6/user/advanced/)
-        # design interface for web version
         # prime status for lowest used price - would need to "click through" to the used/new pages
+
+REFERRAL_ID = '?tag=thithargr-20'
 
 
 def getItemsFromWishListPage(wishlistURL, wishlistID, pageNumber):
@@ -51,7 +53,7 @@ def getItemsFromWishListPage(wishlistURL, wishlistID, pageNumber):
                 continue
             # start with the base URL, add the result from the link in the wishlist item
             # then split off everything after (and including) the first "?"
-            itemURL = 'http://www.amazon.com' + item.find(class_='a-link-normal')["href"] .split("?",1)[0] + '?tag=thithargr-20'
+            itemURL = 'http://www.amazon.com' + item.find(class_='a-link-normal')["href"] .split("?",1)[0]
             itemID = itemURL.split("/")[-1]
             fullPrice = cleanPrice(item.find(id=re.compile('itemPrice_')).text)
 
@@ -142,8 +144,9 @@ def getCategories(itemSoup):
     return categories
 
 
-def getVersions(itemSoup):
-    ''' takes a soup of an item
+
+def getVersions(itemSoup, itemID):
+    ''' takes a soup of an item and that item's ID
         returns an array of dicts with:
             itemVersion - New, Used, Collectible, Hardcover - Used, Hardcover - New, etc 
             versionPrice - 11.96
@@ -151,20 +154,29 @@ def getVersions(itemSoup):
             TODO versionPrimeStatus 
 
     '''
-
     versions = []
+
+    # check to see if it's got the mediatabs layout - if it is, skip for now.
+    if itemSoup.find(id='mediaTabsGroup'):
+        # do nothing 
+        print "Can't handle this layout yet!"
 
     # books will have the little boxes with the 'Hardcover $24.95 | used from $21.45 | New from $22.34' boxes
     # amazon calls those swatches. If they exist, get all the data from them.
-    if itemSoup.find(id='tmmSwatches'):
+    elif itemSoup.find(id='tmmSwatches'):
+        print 'I think this is a book.'
         swatchElements = itemSoup.find(id='tmmSwatches').findAll('li',class_="swatchElement")
         for e in swatchElements:
+
+            # don't care about Audible or Kindle books and they're in a different format
+            if e.text.find('Audible') != -1 or e.text.find('Kindle') != -1:
+                continue
 
             itemType = e.find(class_='a-button-text').find('span').text  # Hardcover
             amazonPrice = cleanPrice(e.find(class_=re.compile('a-color-')).text) # 24.95
             # add the amazon version to the version array
             versions.append({'itemVersion': itemType + ' - Amazon',
-                             'itemPrice': str(amazonPrice)
+                             'itemPrice': amazonPrice
                              })
 
             # all of the different versions will be in spans with classes like 'olp-used', 'olp-new', etc
@@ -175,6 +187,11 @@ def getVersions(itemSoup):
                 # item Type is in the URL on the a element
                 # ex: <a ... href="...ie=UTF8&amp;condition=used&amp;sr=&amp;qid=">
                 #                                 ^^^^^^^^^^^^^^
+
+                # ignore those with blank prices
+                if re.search(r'from \$([0-9]+\.[0-9]+)',str(v.text)) is None:
+                    continue
+
                 itemVersion = re.search(r'condition=([a-zA-Z]+)',str(v.find('a'))).groups(0)[0].capitalize()
                 # price will be in a string like : 6 Used from $21.95
                 itemPrice = cleanPrice(re.search(r'from \$([0-9]+\.[0-9]+)',str(v.text)).groups(0)[0])
@@ -182,6 +199,7 @@ def getVersions(itemSoup):
                                  'itemPrice': itemPrice
                                })
     else:
+        print 'I think this is not a book.'
         # if not, it's probably not a book.
         # this one is a good example of three prices - From Amazon, New, and Used
         # http://www.amazon.com/dp/B004C3CAB8/
@@ -193,24 +211,48 @@ def getVersions(itemSoup):
             versions.append({'itemVersion': 'Amazon',
                              'itemPrice': salePrice
                             })
-        else:
+        elif itemSoup.find(id='priceblock_ourprice'):
             basePrice = cleanPrice(itemSoup.find(id='priceblock_ourprice').text)
             versions.append({'itemVersion': 'Amazon',
                              'itemPrice': basePrice
                             })
-        otherVersions = itemSoup.find(id='olp_feature_div').findAll('span')
-        for s in otherVersions:
-            # TODO - handle multiple colors - ex http://www.amazon.com/dp/B00AIRUOI8/
-            if s.find(class_='a-color-price'):
-                itemType = re.search(r'condition=([a-zA-Z]+)',str(s.find('a'))).groups(0)[0].capitalize()
-                itemPrice = cleanPrice(s.find(class_='a-color-price').text)
-                print 'condition: {0}, price: {1}'.format(itemType, itemPrice)
-                versions.append({'itemVersion': itemType,
-                                'itemPrice': itemPrice
-                                })
+        else:
+            versions.append({'itemVersion': 'Amazon',
+                             'itemPrice': 'Not Found'
+                            })
 
-    print versions
-    return versions
+        # there are at least 2 kinds of layouts:
+        #   - those with normal buyboxes (have an id='olp_feature_div')
+        #   - those with action panels (have an id="actionPanelContainer")
+        
+        if itemSoup.find(id='olp_feature_div'):
+            otherVersions = itemSoup.find(id='olp_feature_div').findAll('span')
+            for s in otherVersions:
+                # TODO - handle multiple colors - ex http://www.amazon.com/dp/B00AIRUOI8/
+                if s.find(class_='a-color-price'):
+                    itemType = re.search(r'condition=([a-zA-Z]+)',str(s.find('a'))).groups(0)[0].capitalize()
+                    itemPrice = cleanPrice(s.find(class_='a-color-price').text)
+                    versions.append({'itemVersion': itemType,
+                                    'itemPrice': itemPrice
+                                    })
+        elif itemSoup.find(id='actionPanelContainer'):
+            ## the only example of this that I've found just has the one price in it
+            # and it's captured by the id='priceblock_ourprice' a couple dozen lines up.
+            # for now, do nothing.
+            pass
+            # actionPanel = itemSoup.find(id='actionPanelContainer')
+            # price = actionPanel.find(id="priceblock_ourprice")
+            # versions.append({'itemVersion': 'Amazon',
+            #                  'itemPrice': 'Not Found'
+            #                 })
+
+        else:
+            print "Can't handle this layout yet!"
+            # TODO - write these to a DB?
+            pass
+
+    addItemVersions(versions, itemID)
+
 
 
 def getInfoFromItemPage(item):
@@ -228,17 +270,15 @@ def getInfoFromItemPage(item):
     primeStatus = getPrimeStatus(itemSoup)
     categories = getCategories(itemSoup)
 
-    # check to see if it's a textbook - if it is, skip for now.
-    if itemSoup.text.find('Try the eTextbook free') != -1:
-        print "Can't handle textbooks yet!"
-        dictToReturn = {"primeStatus": None,
-                        "categories": None,
-                        "versions": None
-                       }
-        return dictToReturn
-    
     # array of dicts - one for each version of the product - hardcover, paperback, kindle, etc
-    versions = getVersions(itemSoup)
+    try:
+        versions = getVersions(itemSoup, item["itemID"])
+    except:
+        f = open('errors/'+item["itemTitle"]+'.html','w')
+        f.write(str(itemSoup))
+        f.close()
+        versions = [{"itemType":'THERE WAS AN ERROR',
+                     "itemPrice": item["itemTitle"]}]
 
     
     dictToReturn = {"primeStatus": primeStatus,
@@ -291,17 +331,6 @@ def isInvalidWishlist(wishListPage):
     else:
         return False
 
-def removeWishlistFromDB(wishlistID):
-    """ remove a wishlist and items from wishlistItems table
-        does not affect anything in the Items table
-    """
-    con = sqlite3.connect('wishlist.db')
-    
-    with con:
-        cur = con.cursor()
-        removeSQL = "delete from wishlistItems where wishlistId = '{0}'".format(wishlistID)
-        cur.execute(removeSQL)
-
 
 def cleanPrice(price):
     ''' takes a string that contains a price
@@ -312,14 +341,17 @@ def cleanPrice(price):
     '''
 
     cleaned_price = price.strip().strip('from ').strip('$')
-    return cleaned_price
+    # if it's anything besides numbers and letters, return "Invalid Price"
+    if re.search('[^a-zA-Z0-9\.]', cleaned_price):
+        cleaned_price = 'Invalid price'
+
+    return str(cleaned_price)
 
 
 
 def refreshWishlist(wishlistID):
 
     # delete all items from wishlist in DB
-
     BASE_URL = 'http://www.amazon.com/gp/registry/wishlist/'
     # connect to wishlist page
     wishlistURL = BASE_URL+wishlistID
@@ -354,14 +386,19 @@ def refreshWishlist(wishlistID):
     for i in range(1, finalPage+1):
         allItems += getItemsFromWishListPage(wishlistURL=BASE_URL, wishlistID=wishlistID, pageNumber=i)
 
+    addWishlistItems(wishlistID, allItems)
+
     # get the info from the item page for each item
     # category, prime status, etc    
     for item in allItems:
         print 'getting info for item ' +  item['itemTitle']
         item.update(getInfoFromItemPage(item))
+        # TODO - get 5 at a time and wait a couple seconds
+        addItemDetails(item)
 
-    #addItemsToDB(allItems)
     print "DONE. Let's add these to a database"
+
+
 
 
 def refreshAllWishlists():
@@ -373,9 +410,9 @@ def refreshAllWishlists():
     con = sqlite3.connect('wishlist.db')
     with con:
         cur = con.cursor()
-        sql = 'select wishlistID from wishlist group by 1;'
+        sql = 'select wishlistID from userWishlists group by 1;'
         cur.execute(sql)
-        data = cur.fetchAll()
+        data = cur.fetchall()
         for d in data:
             wishlists.append(d[0])
 
@@ -383,8 +420,111 @@ def refreshAllWishlists():
         # TODO - can this be parallellized without angering Amazon?
         refreshWishlist(w)
 
+
+
+
+
+
+
+def addWishlistItems(wishlistID, itemsToAdd):
+
+    itemIDs = [item['itemID'] for item in itemsToAdd]
+    dateTrackingStarted = str(datetime.date.today())
+
+    for i in itemIDs:
+        con = sqlite3.connect('wishlist.db')
+        with con:
+            cur = con.cursor()
+            t = (wishlistID, i, dateTrackingStarted)
+            sql = 'insert into wishlistItems (wishlistID, itemID, dateTrackingStarted) VALUES(?,?,?);'
+            cur.execute(sql,t)
+    
+    return True
+    
+
+def addItemDetails(item):
+    ''' take a item dict and add the non-price, non version info to the DB
+        after clearing out the old one. 
+
+    '''
+
+    #clear out the old version
+    con = sqlite3.connect('wishlist.db')
+    with con:
+        cur = con.cursor()
+        t = (item["itemID"],)        
+        cur.execute("delete from itemDetails where itemID = ?", t)
+
+    # add the new one
+    with con:
+        itemInfo = (item["itemID"], item["itemURL"], item["itemTitle"], item["starRating"], item["fullImageURL"], item["thumbnailURL"], item["categories"])
+        cur.execute('insert into itemDetails (itemId, item_url, itemTitle, starRating, fullImageURL, thumbnailURL, categories) VALUES (?,?,?,?,?,?,?)', itemInfo)
+
+    #TODO - error handling
+    return True
+
+
+def addItemVersions(versions, itemID):
+    ''' take an item dict with a list of versions and prices
+        insert them into the itemVersions table
+    '''
+
+    # clear out current data for this item
+    con = sqlite3.connect('wishlist.db')
+    with con:
+        cur = con.cursor()
+        t = (itemID,)   
+        cur.execute("delete from itemVersions where itemID = ?", t)
+
+    # add in the new versions
+
+    with con:
+        for v in versions:
+            t = (itemID, v["itemVersion"], v["itemPrice"])
+            cur.execute('insert into itemVersions (itemID, itemType, itemTypePrice) VALUES (?,?,?)', t)
+
+    #TODO - error handling
+    return True
+
+
+def addUser(userInfo):
+    ''' takes a dict with user name and email
+        adds it to the database
+    '''
+
+    userName = userInfo['name']
+    userEmail = userInfo['email']
+    dateSignedUp = str(datetime.date.today())
+
+    con = sqlite3.connect('wishlist.db')
+    with con:
+        cur = con.cursor()
+        sql = 'insert into users (userName, userEmail, dateSignedUp) VALUES({0},{1},{2});'.format(userName, userEmail, dateSignedUp)
+        cur.execute(sql)
+
+    return True
+
+
+def removeWishlist(wishlistID):
+    """ remove a wishlist and items from wishlistItems table
+        does not affect anything in the Items table
+    """
+    con = sqlite3.connect('wishlist.db')
+    
+    with con:
+        cur = con.cursor()
+        removeSQL = "delete from wishlistItems where wishlistId = '{0}'".format(wishlistID)
+        cur.execute(removeSQL)
+
+    return True
+
+
+
+
+
 def main():
     refreshWishlist('1ZF0FXNHUY7IG')
+
 
 
 if __name__ == "__main__":
