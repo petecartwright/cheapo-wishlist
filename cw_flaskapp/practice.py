32c73e992cc195590c7d1c7f41d8a417f36490db
Pete Cartwright <pete.cartwright@gmail.com>
2015-10-07 21:36:11 -0400

    start bringing this all together

--- cw_flaskapp/practice.py
+++ cw_flaskapp/practice.py
@@ -5,20 +5,21 @@ from wishlist import get_items_from_wishlist, get_wishlist_name
 from datetime import datetime
 
 
-def add_wishlist_items_to_db(wishlist_items):
+def add_wishlist_items_to_db(wishlist, wishlist_items):
     for i in wishlist_items:
         print 'on item ' + i['ASIN']
         # add item to database if it doesn't exist
         item_to_add = Item.query.filter_by(ASIN=i['ASIN']).first()
+        
         if item_to_add is None:
-            print "doesn't exist, creating it"
+            print "i['ASIN'] doesn't exist, creating it"
             item_to_add = Item(ASIN=i['ASIN'])
-            item_to_add.name = i['ASIN'] + 'sadfkasdmfasd'
+            item_to_add.name = i['ASIN'] + ' : Temp Name'
 
         # check to see if it's already in that wishlist, if not, add it
-        if not(item_to_add in w.items):
+        if not(item_to_add in wishlist.items):
             print 'not already in the wishlist... adding'
-            item_to_add.wishlists.append(w)
+            item_to_add.wishlists.append(wishlist)
             db.session.add(item_to_add)
             db.session.commit()
         else:
@@ -27,24 +28,70 @@ def add_wishlist_items_to_db(wishlist_items):
         print 'done with ' + i['ASIN']
 
 
+def get_image_sizes(item_image):
+    """ Amazon API isn't reliable about sending the same fields back every ThumbnailImage
+        This takes an item_image dict and returns what it can get as another dict.
+    """
 
-def main():
+    smallImage = item_image.get('SmallImage')
+    mediumImage = item_image.get('MediumImage')
+    largeImage = item_image.get('LargeImage')
 
-    items = Item.query.all()
-    amazon = get_amazon()
+    if smallImage:
+        smallURL = smallImage.get('URL')
+        smallHeight = smallImage.get('Height')
+        smallWidth = smallImage.get('Width')
+    else:
+        smallURL = ''
+        smallHeight = ''
+        smallWidth = ''
 
-    wishlists = Wishlist.query.all()
+    if mediumImage:
+        mediumURL = mediumImage.get('URL')
+        mediumHeight = mediumImage.get('Height')
+        mediumWidth = mediumImage.get('Width')
+    else:
+        mediumURL = ''
+        mediumHeight = ''
+        mediumWidth = ''
 
-    for w in wishlists:
-        name = get_wishlist_name(w.amazonWishlistID)
-        print 'getting wishlist items'
-        wishlist_items = get_items_from_wishlist(w.amazonWishlistID)
-        print 'got wishlist items'
-        add_wishlist_items_to_db(wishlist_items=wishlist_items)
+    if largeImage:
+        largeURL = largeImage.get('URL')
+        largeHeight = largeImage.get('Height')
+        largeWidth = largeImage.get('Width')
+    else:
+        largeURL = ''
+        largeHeight = ''
+        largeWidth = ''
 
-    # get_item_info(items=items, amazon=amazon)
+    image_sizes = {'smallURL'        : smallURL,
+                   'smallHeight'     : smallHeight,
+                   'smallWidth'      : smallWidth,
+                   'mediumURL'       : mediumURL,
+                   'mediumHeight'    : mediumHeight,
+                   'mediumWidth'     : mediumWidth,
+                   'largeURL'        : largeURL,
+                   'largeHeight'     : largeHeight,
+                   'largeWidth'      : largeWidth}
+
+    return image_sizes
 
 
+def add_variations_to_db(variations):
+    for variation in variations:
+        # check to see if variation exists
+        v = Variation.query.filter_by(parent_ASIN=parent_ASIN).filter_by(ASIN=ASIN).first()
+        if v is None:
+            new_variation = Variation(parent_ASIN=parent_ASIN, 
+                                      ASIN=ASIN)
+            db.session.add(new_variation)
+            db.session.commit()
+
+
+def refresh_all_item_data(items, amazon=None):
+    
+    if amazon is None:
+        amazon = get_amazon()
 
     print 'starting items'
     for i in items:
@@ -69,14 +116,14 @@ def main():
         print 'getting attribs'
         item_attributes = get_item_attributes(ASIN, amazon=amazon)
 
-
-        ## TODO PC - make these return None on error?
         if item_attributes is not None:
-            i.list_price_amount = item_attributes['listPriceAmount']
-            i.list_price_formatted = item_attributes['listPriceFormatted']
-            i.product_group = item_attributes['product_group']
-            i.name = item_attributes['title']
-            i.URL = item_attributes['URL']
+            # using .get() here because it will default to None is the key is
+            # not in the dict, and the API is not reliable about sending everything back
+            i.list_price_amount = item_attributes.get('listPriceAmount')
+            i.list_price_formatted = item_attributes.get('listPriceFormatted')
+            i.product_group = item_attributes.get('product_group')
+            i.name = item_attributes.get('title')
+            i.URL = item_attributes.get('URL')
             i.date_last_checked = datetime.date.today().strftime('%Y-%m-%d')    # today's date as YYYY-MM-DD
 
             db.session.add(i)
@@ -84,7 +131,6 @@ def main():
             print 'got attribs for  ' + ASIN
 
         # get images
-        ## TODO PC - make these return None on error?
         item_images = get_images(ASIN=ASIN, amazon=amazon)
 
         # if we got images back, remove the old ones
@@ -95,32 +141,59 @@ def main():
                 db.commit()
 
         for item_image in item_images:
+            # I wish I could trust the Amazon API to give me good data. 
+            # but I can't.
+            image_sizes = get_image_sizes(item_image)
             new_item_image = Image(# thumbnailURL    = item_image["ThumbnailImage"]["URL"],
                                    # thumbnailHeight = item_image["ThumbnailImage"]["Height"],
-                                   # thumbnailWidth  = item_image["ThumbnailImage"]["Width"],
-                                   smallURL        = item_image["SmallImage"]["URL"],
-                                   smallHeight     = item_image["SmallImage"]["Height"],
-                                   smallWidth      = item_image["SmallImage"]["Width"],
-                                   mediumURL       = item_image["MediumImage"]["URL"],
-                                   mediumHeight    = item_image["MediumImage"]["Height"],
-                                   mediumWidth     = item_image["MediumImage"]["Width"],
-                                   largeURL        = item_image["LargeImage"]["URL"],
-                                   largeHeight     = item_image["LargeImage"]["Height"],
-                                   largeWidth      = item_image["LargeImage"]["Width"],
+                                   # thumbnailWidth  = ite m_image["ThumbnailImage"]["Width"],
+                                   smallURL        = image_sizes['smallURL'],
+                                   smallHeight     = image_sizes['smallHeight'],
+                                   smallWidth      = image_sizes['smallWidth'],
+                                   mediumURL       = image_sizes['mediumURL'],
+                                   mediumHeight    = image_sizes['mediumHeight'],
+                                   mediumWidth     = image_sizes['mediumWidth'],
+                                   largeURL        = image_sizes['largeURL'],
+                                   largeHeight     = image_sizes['largeHeight'],
+                                   largeWidth      = image_sizes['largeWidth'],
                                    item_id         = i.id)
 
-        # get variations
-        ## TODO PC - make these return None on error?
-        variations = get_images(ASIN=ASIN, amazon=amazon)
+        variations = get_variations(ASIN=ASIN, amazon=amazon)
 
-        for variation in variations:
-            # check to see if variation exists
-            v = Variation.query.filter_by(parent_ASIN=parent_ASIN).filter_by(ASIN=ASIN).first()
-            if v is None:
-                new_variation = Variation(parent_ASIN=parent_ASIN, 
-                                          ASIN=ASIN)
-                db.session.add(new_variation)
-                db.session.commit()
+        add_variations_to_db(variations)
+
+        
+
+
+
+def main():
+
+    amazon = get_amazon()
+
+    # pull all existing wishlists
+    wishlists = Wishlist.query.all()
+
+    # for each wishlist, get the name and all items
+    # and push that to the database
+    for w in wishlists:
+        # make sure we have an updated name for the wishlist
+        wishlist_name = get_wishlist_name(w.amazonWishlistID)
+        if wishlist_name is not None:
+            w.name = wishlist_name
+            db.session.add(w)
+            db.session.commit()
+        print 'getting wishlist items'
+        wishlist_items = get_items_from_wishlist(w.amazonWishlistID)
+        print 'got wishlist items'
+        add_wishlist_items_to_db(wishlist=w, wishlist_items=wishlist_items)
+
+    items = Item.query.all()
+
+    for i in items:
+        refresh_item_data(item=i, amazon=amazon)
+
+
+    
 
 
 
