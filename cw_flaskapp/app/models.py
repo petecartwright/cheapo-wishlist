08fb76827cb437e5d1d76f532996a7d4da32a36a
Pete Cartwright <pete.cartwright@gmail.com>
2015-10-12 10:58:13 -0400

    c/p from Flask book to add hashing

--- cw_flaskapp/app/models.py
+++ cw_flaskapp/app/models.py
@@ -1,4 +1,6 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 userWishlists = db.Table('userWishlists', 
     db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
@@ -10,9 +12,24 @@ class User(db.Model):
     id          = db.Column(db.Integer, primary_key=True)
     username    = db.Column(db.String(80), unique=True)
     email       = db.Column(db.String(120), unique=True)
+    password_hash = db.Column(db.String(128))
+
     wishlists   = db.relationship('Wishlist', secondary=userWishlists,
                                 backref=db.backref('users', lazy='dynamic'))
     settings    = db.relationship('UserSettings', backref='user', lazy='dynamic')
+
+    
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
     def __init__(self, username, email):
         self.username = username
