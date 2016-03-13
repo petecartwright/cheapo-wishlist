97501eb57d3729a1962430b5fa3d9acdf6ebec4f
Pete Cartwright <pete.cartwright@gmail.com>
2016-03-13 00:43:44 -0500

    spacing

--- cw_flaskapp/app/__init__.py
+++ cw_flaskapp/app/__init__.py
@@ -7,7 +7,6 @@ from celery import Celery
 app = Flask(__name__)
 app.config.from_object('config')
 
-
 # connect to the DB
 db = SQLAlchemy(app)
 
@@ -25,7 +24,5 @@ app.config['CELERY_RESULT_BACKEND'] = 'redis://'
 
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
-
-
 
 from app import views, models
