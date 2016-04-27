8efc7bd0739f81f0a919b5fdcc088c3a4cad738a
Pete Cartwright <pete.cartwright@gmail.com>
2016-04-26 21:09:19 -0400

    Revert "the purge continues"
    This reverts commit 9f61b420cffd360ce0ffb527f4148484bc72da6e.

--- cw_flaskapp/app/__init__.py
+++ cw_flaskapp/app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from celery import Celery
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -11,5 +13,16 @@ db = SQLAlchemy(app)
 # give the app access to bootstrap templates/css/js/etc
 bootstrap = Bootstrap(app)
 
+# let flask-login handle all the login stuff
+lm = LoginManager(app)
+lm.login_view = 'login'
+lm.login_message = "You need to be logged in to see that page!"
+
+# set up Celery to handle task queues
+app.config['CELERY_BROKER_URL'] = 'redis://'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://'
+
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
 
 from app import views, models
