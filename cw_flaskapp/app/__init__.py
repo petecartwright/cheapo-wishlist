bc7e9a3154b5a4fb346c3afdad42e8b07e36fc4c
Pete Cartwright <pete.cartwright@gmail.com>
2016-05-17 23:21:50 -0400

    remove Flask-Bootstrap

--- cw_flaskapp/app/__init__.py
+++ cw_flaskapp/app/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bootstrap import Bootstrap
-
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -9,7 +7,5 @@ app.config.from_object('config')
 # connect to the DB
 db = SQLAlchemy(app)
 
-# give the app access to bootstrap templates/css/js/etc
-bootstrap = Bootstrap(app)
 
 from app import views, models
