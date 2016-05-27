4db9a56978e100f030c5a27c454971663a932cf1
Pete Cartwright <pete.cartwright@gmail.com>
2016-05-26 22:08:38 -0400

    initial migration

--- cw_flaskapp/migrations/versions/66c98860e322_.py
+++ cw_flaskapp/migrations/versions/66c98860e322_.py
@@ -1,13 +1,13 @@
 """empty message
 
-Revision ID: 297c7feec4c1
+Revision ID: 66c98860e322
 Revises: None
-Create Date: 2016-04-26 21:27:28.856550
+Create Date: 2016-05-25 14:02:55.779492
 
 """
 
 # revision identifiers, used by Alembic.
-revision = '297c7feec4c1'
+revision = '66c98860e322'
 down_revision = None
 
 from alembic import op
@@ -21,12 +21,6 @@ def upgrade():
     sa.Column('parent_ASIN', sa.String(length=40), nullable=True),
     sa.PrimaryKeyConstraint('id')
     )
-    op.create_table('variation',
-    sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('parent_ASIN', sa.String(length=40), nullable=True),
-    sa.Column('ASIN', sa.String(length=40), nullable=True),
-    sa.PrimaryKeyConstraint('id')
-    )
     op.create_table('item',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('ASIN', sa.String(length=40), nullable=True),
@@ -37,6 +31,7 @@ def upgrade():
     sa.Column('product_group', sa.String(length=40), nullable=True),
     sa.Column('date_last_checked', sa.Date(), nullable=True),
     sa.Column('parent_id', sa.Integer(), nullable=True),
+    sa.Column('is_on_wishlist', sa.Boolean(), nullable=True),
     sa.ForeignKeyConstraint(['parent_id'], ['parent_item.id'], ),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('ASIN')
@@ -62,11 +57,14 @@ def upgrade():
     op.create_table('offer',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('condition', sa.String(length=200), nullable=True),
+    sa.Column('offer_source', sa.String(length=100), nullable=True),
     sa.Column('offer_price_amount', sa.Integer(), nullable=True),
     sa.Column('offer_price_formatted', sa.String(length=40), nullable=True),
     sa.Column('prime_eligible', sa.Boolean(), nullable=True),
     sa.Column('availability', sa.String(length=200), nullable=True),
     sa.Column('item_id', sa.Integer(), nullable=True),
+    sa.Column('wishlist_item_id', sa.Integer(), nullable=True),
+    sa.Column('best_offer', sa.Boolean(), nullable=True),
     sa.ForeignKeyConstraint(['item_id'], ['item.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
@@ -78,6 +76,5 @@ def downgrade():
     op.drop_table('offer')
     op.drop_table('image')
     op.drop_table('item')
-    op.drop_table('variation')
     op.drop_table('parent_item')
     ### end Alembic commands ###
