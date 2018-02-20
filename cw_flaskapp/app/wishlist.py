7f2e8975a265144a5956cae3d398a2a8b178f6a4
Pete Cartwright <pete.cartwright@gmail.com>
2018-02-19 21:00:52 -0500

    better logging, correctly load the first page

--- cw_flaskapp/app/wishlist.py
+++ cw_flaskapp/app/wishlist.py
@@ -33,7 +33,7 @@ def get_items_from_wishlist_page(wishlist_id, page_number):
             ASIN = the amazon ASIN for that variation of the item
 
     """
-    print 'getting items from page ' + str(page_number)
+    logger.info'getting items from page ' + str(page_number)
     page_url = "{0}/{1}/?page={2}".format(BASE_URL, wishlist_id, str(page_number))
 
     # connect to wishlist page
@@ -44,11 +44,9 @@ def get_items_from_wishlist_page(wishlist_id, page_number):
     r = requests.get(wishlist_url, headers=headers)
   
     if r.status_code == 200:
-        logger.info('Successful connection to main wishlist page')
+        logger.info('   Successful connection to wishlist page {0}'.format(str(page_number)))
     else:
-        logger.warning('Error connecting to main wishlist page')
-    
-    wishlist_first_page = BeautifulSoup(r.content, "html.parser")
+        logger.warning('    Error connecting to wishlist page {0}'.format(str(page_number)))
 
     wishlist_page = BeautifulSoup(html_data, "html.parser")
 
@@ -57,14 +55,14 @@ def get_items_from_wishlist_page(wishlist_id, page_number):
     item_list = []
     items_on_page = wishlist_page.findAll(id=re.compile('item_'))
     if len(items_on_page) == 0:
-        print 'no items found'
+        logger.info('   no items found')
         return []
     else:
         # each item div in the list has an ID that starts with item_
         for item in items_on_page:
             # if it isn't released yet, we don't need to add it
             if item.find('This title will be released'):
-                print "Not yet out, won't add to DB"
+                logger.info("   Not yet out, won't add to DB")
                 continue
             item_url = 'http://www.amazon.com' + item.find(class_='a-link-normal')["href"].split("?", 1)[0]
             date_added = item.find(class_='dateAddedText').text.strip().split('\n')[0].replace("Added ", "")
@@ -80,26 +78,32 @@ def get_items_from_wishlist(wishlist_id):
     """
     # connect to wishlist page
     wishlist_url = BASE_URL + wishlist_id
-    html_data = get_items_from_wishlist_page(wishlist_url)
-
-    wishlist_first_page = BeautifulSoup(html_data, "html.parser")
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+    }
+    r = requests.get(wishlist_url, headers=headers)
+  
+    if r.status_code == 200:
+        logger.info('   Successful connection to main wishlist page')
+    else:
+        logger.warning('    Error connecting to main wishlist page')
+    
+    wishlist_first_page = BeautifulSoup(r.content, "html.parser")
 
     if wishlist_first_page.find('div', id="wishlistPagination"):
         #if we have multiple pages:
         # get the number of pages on the wishlist. in the pagination div, there's a list of pages to go to.
         # the second to last is the last page of the wishlist. (the last one is "next")
         final_page = int(wishlist_first_page.find('div', id="wishlistPagination").findAll('li')[-2].text)
-        print 'wishlist has {0} pages'.format(final_page)
+        logger.info('wishlist has {0} pages'.format(final_page))
     else:
-        print 'wishlist only has 1 page'
+        logger.info('wishlist only has 1 page')
         final_page = 1
 
     all_items = []
 
-    # only pull one page for testing
-    # final_page = 1
 
-    print 'about to check all pages - have a final page of {0}'.format(str(final_page))
+    logger.info('about to check all pages - have a final page of {0}'.format(str(final_page))
     # run through each page:
     for i in range(1, final_page+1):
         all_items += get_items_from_wishlist_page(wishlist_id=wishlist_id, page_number=i)
@@ -108,11 +112,10 @@ def get_items_from_wishlist(wishlist_id):
     return all_items
 
 
-def main():
-    items = get_items_from_wishlist(PETES_WISHLIST_ID)
-    for i in items:
-        print i
+# def main():
+#     items = get_items_from_wishlist(PETES_WISHLIST_ID)
+    
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
