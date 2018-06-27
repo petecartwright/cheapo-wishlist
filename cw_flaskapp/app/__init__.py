8db9011eaf2e88b8aacb15cdd0b3c0070e713f20
Pete Cartwright <pete.cartwright@gmail.com>
2018-06-27 11:51:03 -0400

    add CORS support

--- cw_flaskapp/app/__init__.py
+++ cw_flaskapp/app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
-
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
 mail = Mail(app)
+CORS(app)
 
 from app import views, models
